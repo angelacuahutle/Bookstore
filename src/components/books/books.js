52a67506229b1a4b8253adde1821fcd3fe6d09ba
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-25 16:41:06 -0600

    Add Books, fix navbar

--- src/components/books/books.js
+++ src/components/books/books.js
@@ -2,12 +2,79 @@ import React from 'react';
 
 function Books() {
   return (
-    <div>
-      <table>
-        <span> &quot;Book&quot; 1 by &quot;Author1&quot;  </span>
-      </table>
-    </div>
+    <>
+      <div className="d-flex justify-content-center mt-5">
+        <form>
+          <h2>Add a Book</h2>
+          <input className="form-control" type="text" placeholder="Write a book" />
+          <input className="form-control" type="text" placeholder="Write a Authot" />
+        </form>
+      </div>
+      <div className="d-flex justify-content-center mt-5">
+        <table>
+          <tr>
+            <th>
+              <b>
+                &quot;Exquisite Mariposa: A Novel&quot;
+              </b>
+            </th>
+            <th>by</th>
+            <th>&quot; Fiona Alison Duncan&quot;</th>
+            <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+          </tr>
+          <tr>
+            <th>
+              <b>
+                &quot;Exquisite Mariposa: A Novel&quot;
+              </b>
+            </th>
+            <th>by</th>
+            <th>&quot; Fiona Alison Duncan&quot;</th>
+            <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+          </tr>
+          <tr>
+            <th>
+              <b>
+                &quot;Exquisite Mariposa: A Novel&quot;
+              </b>
+            </th>
+            <th>by</th>
+            <th>&quot;Fiona Alison Duncan&quot;</th>
+            <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+          </tr>
+          <tr>
+            <th>
+              <b>
+                &quot;Gifts&quot;
+              </b>
+            </th>
+            <th>by</th>
+            <th>&quot;Ursula K. Le Guin&quot;</th>
+            <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+          </tr>
+          <tr>
+            <th>
+              <b>
+                &quot;Gifts&quot;
+              </b>
+            </th>
+            <th>by</th>
+            <th>&quot;Ursula K. Le Guin&quot;</th>
+            <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+          </tr>
+          <tr>
+            <th>
+              <b>
+                &quot;Oval&quot;
+              </b>
+            </th>
+            <th>by</th>
+            <th>&quot; Elvia Wilk&quot;</th>
+
+            <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+          </tr>
+        </table>
+      </div>
+    </>
   );
 }
-
-export default Books;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Bookstore</Link>
-      <div>
-        <Link to="/">Books</Link>
+    <nav className="p-2 navbar d-flex justify-content-between">
+      <div className="display-4 ">
+        <Link to="/" className="text-black text-decoration-none p-3">
+          Bookstore
+        </Link>
+        <FontAwesomeIcon icon={faBook} />
       </div>
+      <Link to="/" className="text-black text-decoration-none">Books</Link>
+      <br />
+      <Link to="/categories">Categories</Link>
+      <Link to="/categories" className="text-black text-decoration-none">Categories</Link>
     </nav>
   );
 }
