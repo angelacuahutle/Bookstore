726a4c6ba508a6b1267caf95c09b4700525cb9dc
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-25 16:02:19 -0600

    Add boostsrap, caategories and Navbar and link to books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+import Books from './components/books/books';
+import 'bootstrap/dist/css/bootstrap.css';
+import Categories from './components/categories/categories';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,10 +15,12 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
         </Routes>
       </BrowserRouter>
     );
   }
 }
+
 export default App();
---
+++ src/components/books/books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Books() {
+  return (
+    <div>
+      <table>
+        <span> &quot;Book&quot; 1 by &quot;Author1&quot;  </span>
+      </table>
+    </div>
+  );
+}
+
+export default Books;
