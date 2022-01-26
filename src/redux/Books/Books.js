86a6bd6f306dc718636b4c0afaa2981e250d67b6
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-26 16:11:23 -0600

    Add necessary files, book reducer and dependencies needed

---
+++ src/redux/Books/Books.js
@@ -0,0 +1,26 @@
+const ADD_BOOK = './Books/ADD_BOOK';
+const REMOVE_BOOK = './Books/REMOVE_BOOK';
+const initialState = [];
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
