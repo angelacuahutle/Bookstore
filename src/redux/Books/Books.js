8b4394a617bcde7c70b7b6411294b5fbb2b19376
angelacuahutle <angela.cuahutle@gmail.com>
2022-01-27 14:06:13 -0600

    Add getBooks from API function and add books reducer

--- src/redux/Books/Books.js
+++ src/redux/Books/Books.js
@@ -1,5 +1,6 @@
 const ADD_BOOK = './Books/ADD_BOOK';
 const REMOVE_BOOK = './Books/REMOVE_BOOK';
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
 const initialState = [];
 
 export const addBook = (payload) => ({
@@ -12,12 +13,24 @@ export const removeBook = (id) => ({
   id,
 });
 
+export const getBook = (payload) => ({
+  type: GET_BOOKS,
+  payload,
+});
+
+export const getBooksAsync = () => async (dispatch) => {
+  const data = await getBook();
+  dispatch(getBook(data));
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
+    case GET_BOOKS:
+      return action.payload;
     default:
       return state;
   }
